ae2c0d9ce587d411366040d9060e58a6b653c85d
guavacoderepo <guavacodeplus@gmail.com>
2023-05-13 20:18:11 +0100

    new commit

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
-const express = require()
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+// load dot env file
+dotenv.config({ path: ".config/config.env" });
+
+const app = express();
+
+// import middlewares
+app.use(express.json());
+app.use(cors());
+
+// set port
+const port = process.env.PORT || 3001;
+
+// listen to req
+app.listen(port, () => {
+  console.log(`connected to port ${port}`);
+});
+
+// connect to mongo db
+connectDB();
